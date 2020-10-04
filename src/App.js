907e56e92be0ffec21b2c2f11cfc58dd5409a454
jethrosama <kel73896@gmail.com>
2020-10-04 22:24:55 +0800

    add about section

--- src/App.js
+++ src/App.js
@@ -19,15 +19,26 @@ function App() {
           <h1 className='app__introText'>Let's build something together</h1>
         </div>
       </div>
-
-      {/* about section */}
-      {/* - about */}
-      {/* Big text */}
-      {/* Email text */}
-      {/* social links */}
-      {/* linkedIn */}
-      {/* github */}
-
+      <div className='app__about'>
+        <div className='app__aboutContainer'>
+          <div className='app__aboutTag'>- about</div>
+          <div className='app__aboutContainer2'>
+            <h1 className='app__aboutText'>
+              Lorem ipsum Lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+              lorem
+            </h1>
+            <h2 className='app__aboutEmail'>Natividad.jethro69@gmail.com</h2>
+          </div>
+          <div className='app__aboutSocial'>
+            {[
+              { display: 'LinkedIn', link: '#' },
+              { display: 'Github', link: '#' },
+            ].map((item) => (
+              <a href={item.link}>{item.display}</a>
+            ))}
+          </div>
+        </div>
+      </div>
       {/* Projects section */}
       {/* - projects */}
       {/* List of projects */}
