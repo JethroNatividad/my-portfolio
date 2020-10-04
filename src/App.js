993df522bda8d0ae7055f1f3eacb4972a6eca9d0
jethrosama <kel73896@gmail.com>
2020-10-04 21:31:14 +0800

    drafted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,55 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='app'>
+      {/* Header */}
+      {/* Logo */}
+      {/* about */}
+      {/* projects */}
+      {/* skills */}
+      {/* contact */}
+
+      {/* intro */}
+      {/* Logo */}
+      {/* Name */}
+      {/* some text */}
+
+      {/* about section */}
+      {/* - about */}
+      {/* Big text */}
+      {/* Email text */}
+      {/* social links */}
+      {/* linkedIn */}
+      {/* github */}
+
+      {/* Projects section */}
+      {/* - projects */}
+      {/* List of projects */}
+      {/* Flippable card Project */}
+      {/* Front: */}
+      {/* IMAGE */}
+      {/* Back: */}
+      {/* title */}
+      {/* description */}
+      {/* technologies used */}
+      {/* preview button */}
+      {/* see live button */}
+      {/* source code button */}
+
+      {/* Skills section */}
+      {/* Skills list */}
+      {/* technologies list */}
+      {/* some text */}
+
+      {/* Contact section */}
+      {/* Form */}
+      {/* username */}
+      {/* email */}
+      {/* message */}
+      {/* send button */}
+    </div>
+  );
 }
 
 export default App;
