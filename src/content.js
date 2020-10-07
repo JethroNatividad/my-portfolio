3dae5e6bed80f296858128bdddd71901cf26f022
jethrosama <kel73896@gmail.com>
2020-10-07 22:03:04 +0800

    hosted images on imgur

--- src/content.js
+++ src/content.js
@@ -6,8 +6,7 @@ export const ProjectsData = [
       'A realtime dynamic Covid-19 tracker app with a full functional map, live cases, countries and graph made with React.js and disease.sh api',
     tech:
       'React.js, disease.sh api, css, javascript, material-ui, leaflet, chart.js',
-    imageUrl:
-      'https://s3.amazonaws.com/awesomescreenshot/upload/1318047/1358412/4e5422c3-9055-4c9e-5ad1-d42638553ffb.png?AWSAccessKeyId=AKIAJSCJQ2NM3XLFPVKA&Expires=1602062734&Signature=7mHHodHVHj1pxObW26b40TUSKEc%3D',
+    imageUrl: 'https://i.imgur.com/ybRXdrc.png',
     gif: 'https://s8.gifyu.com/images/COVID-19-Tracker-_2_.gif',
     siteUrl: 'https://covid-trackin.web.app/',
     github: 'https://github.com/JethroSama/covid-tracker',
@@ -17,8 +16,7 @@ export const ProjectsData = [
     description:
       "A Google search clone with search functionality made with React.js and google's custom search",
     tech: 'React.js, custom search api, css, javascript, material-ui',
-    imageUrl:
-      'https://s3.amazonaws.com/awesomescreenshot/upload/1318047/1358412/c53e921d-9c0e-47fc-4072-c42e8bf396d5.png?AWSAccessKeyId=AKIAJSCJQ2NM3XLFPVKA&Expires=1602064503&Signature=%2Flhw90gZiaMURXxTp1AraB3CxfI%3D',
+    imageUrl: 'https://i.imgur.com/WSLTuCW.png',
     gif: 'https://s8.gifyu.com/images/Google-Clone.gif',
     siteUrl: 'https://goooogl-e.web.app/',
     github: 'https://github.com/JethroSama/google-clone',
@@ -28,8 +26,7 @@ export const ProjectsData = [
     description:
       'A Movie browsing application with search, view, and pagination functionality made with React.js',
     tech: 'React.js, omdb api, html, css, javascript, material-ui',
-    imageUrl:
-      'https://s3.amazonaws.com/awesomescreenshot/upload/1318047/1358412/cbe71091-3215-4385-76ed-7957730af76c.png?AWSAccessKeyId=AKIAJSCJQ2NM3XLFPVKA&Expires=1602065915&Signature=5i4D9wa3WfbCUlQQDSO0WfWxfbI%3D',
+    imageUrl: 'https://i.imgur.com/97G54NC.png',
     gif: 'https://s8.gifyu.com/images/Movie-Hut-_2_.gif',
     siteUrl: 'https://movie-hut.netlify.app/',
     github: 'https://github.com/JethroSama/MovieHub',
@@ -39,8 +36,7 @@ export const ProjectsData = [
     description:
       'A bible reading app with many bible versions made with React.js',
     tech: 'React.js, bible api, html, css, javascript, bootstrap',
-    imageUrl:
-      'https://s3.amazonaws.com/awesomescreenshot/upload/1318047/1358412/e299c9c6-9d2f-47f1-486c-79a7ecd796a8.png?AWSAccessKeyId=AKIAJSCJQ2NM3XLFPVKA&Expires=1602066779&Signature=qUbURJAUAojlGf6AvgSDe%2BbJmFA%3D',
+    imageUrl: 'https://i.imgur.com/B3c0SkS.png',
     gif: 'https://s8.gifyu.com/images/SBIBLE-_2_.gif',
     siteUrl: 'https://sbible.netlify.app/',
     github: 'https://github.com/JethroSama/Sbible',
@@ -50,8 +46,7 @@ export const ProjectsData = [
     description:
       'A Full stack Posting app with user authentication, admin authorization,Post and comment with full CRUD functionality, user post list, ',
     tech: 'Node.js, Mongodb, express, bootstrap, ejs, css, html',
-    imageUrl:
-      'https://s3.amazonaws.com/awesomescreenshot/upload/1318047/1358412/b1826e2e-8cdb-4101-4260-3a95c8846c59.png?AWSAccessKeyId=AKIAJSCJQ2NM3XLFPVKA&Expires=1602069647&Signature=PvU%2BD21Lv6oj8GGz6HZpHbB0V%2BA%3D',
+    imageUrl: 'https://i.imgur.com/TUvFYCk.png',
     gif: 'https://s8.gifyu.com/images/PostHub.gif',
     siteUrl: 'https://posthub-com.herokuapp.com/',
     github: 'https://github.com/JethroSama/PostHub',
@@ -61,8 +56,7 @@ export const ProjectsData = [
     description:
       'A fullstack Instagram clone with user auth, post and, comment functionality made with React.js and Firebase',
     tech: 'React.js, Firebase, css, html, material-ui',
-    imageUrl:
-      'https://s3.amazonaws.com/awesomescreenshot/upload/1318047/1358412/b9ba2f4f-e373-46b7-66fe-97a6766d9ae4.png?AWSAccessKeyId=AKIAJSCJQ2NM3XLFPVKA&Expires=1602072620&Signature=ITrXFMzPZimsCvxu%2FA%2BcXJGGJWo%3D',
+    imageUrl: 'https://i.imgur.com/JpmLGhX.png',
     gif: 'https://s8.gifyu.com/images/React-App.gif',
     siteUrl: 'https://instagra-m-clone.web.app/',
     github: 'https://github.com/JethroSama/instagram-clone',
